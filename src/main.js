6ae3170fbd1ae78bc775e42af95fdb3ba3a3e096
Socrates of Athens <ofathenss@gmail.com>
2020-08-05 23:58:17 +0200

    Set resolution

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import HelloWorldScene from './scenes/HelloWorldScene'
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1600,
+  height: 900,
   physics: {
     default: 'arcade',
     arcade: {
