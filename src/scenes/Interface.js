245b89e6c2434d1c4e627370c509959bfd57055f
Socrates of Athens <ofathenss@gmail.com>
2020-08-06 15:12:25 +0200

    Wrap text

--- src/scenes/Interface.js
+++ src/scenes/Interface.js
@@ -62,6 +62,21 @@ export default class Interface extends Phaser.Scene {
       .setInteractive()
       .on('pointerdown', () => console.log('$'))
 
+    this.add.rectangle(0, 748, 18, 18, 0xFF0000).setOrigin(0, 0)
+
+    this
+      .add
+      .text(
+        18,
+        728,
+        "I need your semen for a science experiment. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        {
+          fontFamily: 'futura',
+          fontSize: '31px',
+          wordWrap: { width: 1314, height: 150 }
+        }
+      )
+
     paper.on(
       'pointerup',
       () => console.log('test')
--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -12,13 +12,16 @@ export default class Title extends Phaser.Scene {
   create = () => {
     this.physics.add.image(800, 450, 'logo')
 
+    const advance = () => {
+      this.scene.start('interface')
+    }
+
     this
       .input
-      .on(
-        'pointerup',
-        () => this.scene.start('interface'),
-        this
-      )
+      .on('pointerup', advance, this)
+
+    const space = this.input.keyboard.addKey('SPACE')
+    space.on('down', advance)
 
     this.add.text(0, 0, '', { font: 'futura' })
 
