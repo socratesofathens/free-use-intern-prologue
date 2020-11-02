11cce84b1214f285edcf1ee9b616cdeedea628e4
Socrates of Athens <ofathenss@gmail.com>
2020-11-02 10:42:16 +0100

    Build electron

--- src/lib/game.js
+++ src/lib/game.js
@@ -2,6 +2,26 @@ export const GAME_WIDTH = 3911
 
 export const GAME_HEIGHT = 2200
 
+export function realWidth (scalar) {
+  return scalar * GAME_WIDTH
+}
+
+export function realHeight (scalar) {
+  return scalar * GAME_HEIGHT
+}
+
+export function realSize (size) {
+  const { width, height } = size
+
+  const realizedWidth = realWidth(width)
+  const realizedHeight = realHeight(height)
+
+  return {
+    width: realizedWidth,
+    height: realizedHeight
+  }
+}
+
 export const GAME_SIZE = {
   width: GAME_WIDTH,
   height: GAME_HEIGHT
